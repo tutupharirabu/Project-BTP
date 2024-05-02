09d29760dc38e3eedcc2bde80d80789576bed9b2
Hafiz Yazid Muhammad <97972505+hafizyzd@users.noreply.github.com>
2024-05-02 22:43:36 +0700

    status pengajuan ruangan admin

--- prototype-BTP/resources/views/admin/layouts/mainAdmin.blade.php
+++ prototype-BTP/resources/views/admin/layouts/mainAdmin.blade.php
@@ -8,8 +8,7 @@
     <title>Peminjaman Bandung Techno Park</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <!-- untuk logo check, cancel dan wait di status ruangan admin -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
@@ -26,7 +25,7 @@
 
     <div class="row d-flex min-vh-100">
         @include('penyewa.partials.sidebarUser')
-        <div class="col-md-9">
+        <div class="col-md-10">
             @yield('containAdmin')
         </div>
     </div>
