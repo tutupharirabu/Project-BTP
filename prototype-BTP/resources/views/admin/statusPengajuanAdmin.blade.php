1ec21aa19dd2e3eb78c6e30be3ae5a011f87c076
Hafiz Yazid Muhammad <97972505+hafizyzd@users.noreply.github.com>
2024-07-31 22:29:47 +0700

    filter tabel

--- prototype-BTP/resources/views/admin/statusPengajuanAdmin.blade.php
+++ prototype-BTP/resources/views/admin/statusPengajuanAdmin.blade.php
@@ -184,7 +184,7 @@
                                                     @endforeach
                                                 </td>
                                                 <td>
-                                                    <button type="button" class="btn btn-success btn-sm text-capitalize"style=""
+                                                    <button type="button" class="btn btn-success btn-md text-capitalize"style="background-color:#0C9300;"
                                                         data-bs-toggle="modal"
                                                         data-bs-target="#exampleModal{{ $data->id_peminjaman }}">
                                                         Catatan
@@ -420,7 +420,7 @@
                     $('#dataTPengajuan').DataTable({
                         "paging": true,
                         "searching": true,
-                        "ordering": true,
+                        "ordering": false,
                         "info": true
                     });
                 });
